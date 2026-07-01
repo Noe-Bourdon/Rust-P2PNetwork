1b3b390a56eb11c135a58e33425e809741204cb8
Noe-Bourdon <noe.bourdon.jp@gmail.com>
2026-07-01 22:28:07 +0900

    ゴシップサブ追加

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
-use libp2p::futures::StreamExt as _;
-use std::error::Error;
+use libp2p::futures::{AsyncBufReadExt, StreamExt as _, io};
+use std::hash::{Hash, Hasher};
+use std::{error::Error, hash::DefaultHasher};
 use std::time::Duration;
+use libp2p::{gossipsub, request_response};
+struct MyCodec;
 
 #[derive(libp2p::swarm::NetworkBehaviour)]
 struct Behaviour {
     mdns: libp2p::mdns::tokio::Behaviour,
     ping: libp2p::ping::Behaviour,
+    gossipsub: libp2p::gossipsub::Behaviour
 }
 
+use tokio::io::stdin;
+
 impl Behaviour {
-    pub fn new(mdns: libp2p::mdns::tokio::Behaviour, ping: libp2p::ping::Behaviour) -> Self {
-        Self { mdns, ping }
+    pub fn new(mdns: libp2p::mdns::tokio::Behaviour, ping: libp2p::ping::Behaviour, gossipsub: libp2p::gossipsub::Behaviour ) -> Self {
+        Self { mdns, ping, gossipsub }
     }
 }
 
@@ -35,11 +41,32 @@ async fn main(
                         .with_timeout(Duration::from_secs(5))
                         .with_interval(Duration::from_secs(1)),
                 );
-                Behaviour::new(mdns, ping)
+                let message_id_fn = |message: &gossipsub::Message| {
+                    let mut s = DefaultHasher::new();
+                    message.data.hash(&mut s);
+                    gossipsub::MessageId::from(s.finish().to_string())
+                };
+                let gossipsub_config = gossipsub::ConfigBuilder::default()
+                    .heartbeat_interval(Duration::from_secs(10))
+                    .validation_mode(gossipsub::ValidationMode::Strict)
+                    .message_id_fn(message_id_fn)
+                    .build().unwrap();
+                let gossipsub = gossipsub::Behaviour::new(
+                    gossipsub::MessageAuthenticity::Signed(keypair.clone()),
+                    gossipsub_config, 
+                ).unwrap();
+                Behaviour::new(mdns, ping, gossipsub)
 			}
 		)?
         .with_swarm_config(|config|config.with_idle_connection_timeout(Duration::from_secs(5)))
 		.build();
+
+    // topic error 
+    let topic = gossipsub::IdentTopic::new("test-net");
+    swarm.behaviour_mut().gossipsub.subscribe(&topic).unwrap();
+
+    let mut stdin = io::BufReader::new(io::stdin()).lines();
+
     println!("peer id {}", swarm.local_peer_id());
 	swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 	loop{
